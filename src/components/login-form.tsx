4b67fcfc88ab9b7dd52daeef6c35d24389cae345
subodh-06 <subodhkumarpradhan63@gmail.com>
2025-09-22 03:39:10 +0530

    add a toggle button to see password as text in login

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -14,6 +14,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Cookies from "js-cookie"
+import { Eye, EyeOff } from "lucide-react"
 
 export function LoginForm({
   className,
@@ -23,6 +24,7 @@ export function LoginForm({
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [loading, setLoading] = useState(false)
+  const [showPassword, setShowPassword] = useState(false)
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
@@ -45,11 +47,11 @@ export function LoginForm({
 
         if (data.role === "staff") {
           router.push("/faculty")
-        } 
+        }
         else if (data.role === "student") {
           router.push("/student")
         }
-        else if(data.role==="admin"){
+        else if (data.role === "admin") {
           router.push("/admin")
         }
       } else {
@@ -98,14 +100,23 @@ export function LoginForm({
                     >
                       Forgot your password?
                     </a>
-                  </div>
-                  <Input
+                  </div >
+                  <div className="relative">
+                    <Input
                     id="password"
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                   />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                  >
+                    {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                  </button>
+                  </div>
                 </div>
                 <Button type="submit" disabled={loading} className="w-full">
                   {loading ? "Logging in..." : "Login"}
